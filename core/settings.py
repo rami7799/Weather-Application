49f086fb91b5bfd730b3d75eac4f5f9f78cb85bd
Ramyar Arefi <arefi_ramyar99@yahoo.com>
2022-07-14 17:41:26 +0430

    hrtrhrt

--- core/settings.py
+++ core/settings.py
@@ -78,12 +78,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'ciba',
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
